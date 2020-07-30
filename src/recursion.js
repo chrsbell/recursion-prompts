a639900e25c17a4cb5c5c08a89dc2d22cfd585ba
Chris Bell <cb.camellia@gmail.com>
2020-07-29 21:22:34 -0400

    Implement isEven

--- src/recursion.js
+++ src/recursion.js
@@ -44,6 +44,12 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if (n === 1) {
+    return false;
+  } else if (n === 0) {
+    return true;
+  }
+  return isEven(Math.abs(n) - 2);
 };
 
 // 5. Sum all integers below a given integer.
